b338fee08755b20c453b4a210d52134553ad4884
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-10 22:00:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,108 +9,90 @@ stamps = []
 # Home page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', stamps=stamps)
 
-# Add a new stamp to the collection
-@app.route('/add_stamp', methods=['POST'])
-def add_stamp():
+# Add a new stamp
+@app.route('/add', methods=['POST'])
+def add():
     stamp = request.form.get('stamp')
     stamps.append(stamp)
     return redirect(url_for('index'))
 
-# Edit an existing stamp in the collection
-@app.route('/edit_stamp/<int:stamp_id>', methods=['POST'])
-def edit_stamp(stamp_id):
+# Edit an existing stamp
+@app.route('/edit/<int:id>', methods=['POST'])
+def edit(id):
     stamp = request.form.get('stamp')
-    stamps[stamp_id] = stamp
+    stamps[id] = stamp
     return redirect(url_for('index'))
 
-# Delete a stamp from the collection
-@app.route('/delete_stamp/<int:stamp_id>')
-def delete_stamp(stamp_id):
-    del stamps[stamp_id]
+# Delete a stamp
+@app.route('/delete/<int:id>')
+def delete(id):
+    del stamps[id]
     return redirect(url_for('index'))
 
-# View all of the stamps in the collection
-@app.route('/view_stamps')
-def view_stamps():
-    return render_template('view_stamps.html', stamps=stamps)
+# View the stamps
+@app.route('/view')
+def view():
+    return render_template('view.html', stamps=stamps)
 
-# Search for stamps in the collection
-@app.route('/search_stamps', methods=['POST'])
-def search_stamps():
-    search_term = request.form.get('search_term')
-    results = [stamp for stamp in stamps if search_term in stamp]
-    return render_template('view_stamps.html', stamps=results)
+# Search for stamps
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form.get('query')
+    results = [stamp for stamp in stamps if query in stamp]
+    return render_template('search.html', results=results)
 
 if __name__ == '__main__':
     app.run()
 
 
-HTML code for index.html
-
-html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Stamp Collection</title>
-</head>
-<body>
-    <h1>Stamp Collection</h1>
-    <form action="/add_stamp" method="post">
-        <input type="text" name="stamp" placeholder="Add a new stamp">
-        <input type="submit" value="Add">
-    </form>
-    <ul>
-        {% for stamp in stamps %}
-            <li>{{ stamp }}</li>
-        {% endfor %}
-    </ul>
-    <a href="/view_stamps">View all stamps</a>
-    <a href="/search_stamps">Search for stamps</a>
-</body>
-</html>
+main.py file:
 
 
-HTML code for view_stamps.html
+from flask import Flask, render_template, request, redirect, url_for
 
-html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>View Stamps</title>
-</head>
-<body>
-    <h1>View Stamps</h1>
-    <ul>
-        {% for stamp in stamps %}
-            <li>{{ stamp }}</li>
-        {% endfor %}
-    </ul>
-    <a href="/">Home</a>
-</body>
-</html>
+app = Flask(__name__)
 
+# Create a database to store the stamps
+stamps = []
 
-HTML code for search_stamps.html
+# Home page
+@app.route('/')
+def index():
+    return render_template('index.html', stamps=stamps)
 
-html
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Search Stamps</title>
-</head>
-<body>
-    <h1>Search Stamps</h1>
-    <form action="/search_stamps" method="post">
-        <input type="text" name="search_term" placeholder="Search for a stamp">
-        <input type="submit" value="Search">
-    </form>
-    <ul>
-        {% for stamp in stamps %}
-            <li>{{ stamp }}</li>
-        {% endfor %}
-    </ul>
-    <a href="/">Home</a>
-</body>
-</html>
+# Add a new stamp
+@app.route('/add', methods=['POST'])
+def add():
+    stamp = request.form.get('stamp')
+    stamps.append(stamp)
+    return redirect(url_for('index'))
+
+# Edit an existing stamp
+@app.route('/edit/<int:id>', methods=['POST'])
+def edit(id):
+    stamp = request.form.get('stamp')
+    stamps[id] = stamp
+    return redirect(url_for('index'))
+
+# Delete a stamp
+@app.route('/delete/<int:id>')
+def delete(id):
+    del stamps[id]
+    return redirect(url_for('index'))
+
+# View the stamps
+@app.route('/view')
+def view():
+    return render_template('view.html', stamps=stamps)
+
+# Search for stamps
+@app.route('/search', methods=['POST'])
+def search():
+    query = request.form.get('query')
+    results = [stamp for stamp in stamps if query in stamp]
+    return render_template('search.html', results=results)
+
+if __name__ == '__main__':
+    app.run()
